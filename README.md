060f1f806e80072c96f8bbdf97dbcdb3e6e7c3f0
Alexander Sayner <alexandr.DEF@mail.ru>
2019-08-07 11:35:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Spring Securiry example project
---
+++ src/main/java/sayner/sandbox/MainClass.java
@@ -0,0 +1,13 @@
+package sayner.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClass {
+
+    // Входная точка приложения
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+    }
+}
